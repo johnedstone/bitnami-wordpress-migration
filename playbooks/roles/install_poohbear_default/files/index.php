361ed7ce07b3cb879c36b655d6d01fceece93dff
johnedstone <johnedstone@gmail.com>
2021-05-20 00:10:53 +0000

    working on default pooh bear

---
+++ playbooks/roles/install_poohbear_default/files/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Pooh Bear</title>
+  <meta name="description" content="Winnie the Pooh">
+  <meta name="author" content="johnedstone">
+
+</head>
+
+<body>
+  <div style="color: #adbce6;font-size:1.2em;font-family: sans-serif;">Pooh Bear!</div>
+</body>
+</html>
+
